7ff70e6722582b929d28a896bbb2d4f5cbffef66
mlgmontage <kettleofrassel1@gmail.com>
2020-11-06 18:00:30 +0500

    comment section

--- tweet.js
+++ tweet.js
@@ -1,1 +1,33 @@
-console.log('tweet individual')
+const tweetId = location.hash.slice(1)
+const host = `http://localhost:8080/`
+const commentsBlock = document.querySelector('#commentsBlock')
+
+console.log(tweetId)
+
+const fetchComments = async () => {
+  const response = await fetch(`${host}api/routes/comments/${tweetId}`)
+  const comments = await response.json()
+
+  console.log(comments)
+
+  comments.forEach(comment => {
+    commentsBlock.innerHTML += `
+      <figure class="mb-3">
+
+        <blockquote class="blockquote mb-4">
+          <div class="text-dark text-decoration-none">${comment.Comment}</div>
+        </blockquote>
+
+        <figcaption class="blockquote-footer">
+          <span class="text-black">${comment.name} ${comment.lastname}</span> |
+          <a href="#" class="text-muted">@${comment.login}</a>
+        </figcaption>
+
+        <hr />
+
+      </figure>
+    `
+  })
+}
+
+fetchComments()
