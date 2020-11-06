812ca35782b1e01cf9b1b7d84221a0b1cb0a3feb
mlgmontage <kettleofrassel1@gmail.com>
2020-11-06 15:33:00 +0500

    tweets list

--- tweets.js
+++ tweets.js
@@ -1,1 +1,27 @@
-console.log("tweets")
+console.log("tweet")
+const tweetsBlock = document.querySelector("#tweetsBlock")
+const host = `http://localhost:8080/`
+
+const fetchTweets = async () => {
+  const response = await fetch(`${host}api/routes/tweets`)
+  const tweets = await response.json()
+  console.log(tweets)
+
+  tweets.forEach(tweet => {
+    tweetsBlock.innerHTML += `
+            <figure class="mb-3">
+
+              <blockquote class="blockquote mb-4">
+                <a href="" class="text-dark text-decoration-none">${tweet.Tweet}</a>
+              </blockquote>
+
+              <figcaption class="blockquote-footer">
+                <a href="" class="text-muted">${tweet.name} ${tweet.lastname}</a>
+              </figcaption>
+
+            </figure>
+   `;
+  })
+}
+
+fetchTweets()
